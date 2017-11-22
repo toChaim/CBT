d19aa9cb083df2e2247dc589dc90d4f8c0b272c2
toChaim <toChaim@gmail.com>
2017-11-21 22:45:33 -0800

    refactor server.

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+// modules
 const express = require('express');
+const router = express.Router();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// config settings
+const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost/CBT-API';
+const port = process.env.PORT || 8000;
+
+// set api
 const api = express();
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/CBT-API');
-
-const port = process.env.PORT || 8000;
-
-const router = express.Router();
-
+// set routs
 router.get('/', (req, res) => res.json({ message: 'welcome to your mind.' }));
-
 api.use('/api', router);
 
-api.listen(port);
-console.log(`CBT-API listening on port:${port}`);
+// connect to db
+mongoose.connect(mongoDB, { useMongoClient: true });
+
+api.listen(port, () => {
+  console.log(`CBT-API listening on port:${port}`);
+});
