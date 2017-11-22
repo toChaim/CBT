288925c90cd01833a26e38aa63cd3c62fe0d1ce0
toChaim <toChaim@gmail.com>
2017-11-21 18:49:40 -0800

    adding mongoose.

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const api = express();
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const api = express();
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/CBT-API');
 
 const port = process.env.PORT || 8000;
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.json({ message: "welcome to your mind." }));
+router.get('/', (req, res) => res.json({ message: 'welcome to your mind.' }));
 
-api.use("/api", router);
+api.use('/api', router);
 
 api.listen(port);
 console.log(`CBT-API listening on port:${port}`);
